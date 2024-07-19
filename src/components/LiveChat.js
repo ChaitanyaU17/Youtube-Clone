fc6fbc443ebc9786084869d3d50a2cc363e9ff7f
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-07-19 14:45:16 +0530

    render chat

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,39 +1,66 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import ChatMessage from './ChatMessage'
 import { addMessage } from '../utils/chatSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
+import { generateRandomName, makeRandomMessage } from '../utils/helper';
+import { IoSend } from "react-icons/io5";
 
 
 const LiveChat = () => {
+  const [liveMessage, setLiveMessage] = useState();
 
     const dispatch = useDispatch();
 
     const chatMessages = useSelector((store) => store.chat.messages);
-    const id = uuidv4();
 
     useEffect(() => {
        const i = setInterval(() => {
             //api polling
-            console.log('api polling');
+            // console.log('api polling');
 
             dispatch(addMessage({
-                name: 'chaitanya',
-                message: 'hello live 🔴',
+              id: uuidv4(),
+              name: generateRandomName(),
+              message: makeRandomMessage(20),
             })
         );
-        }, 2000);
+        }, 3000);
         
         return () => clearInterval(i);
     }, []);
 
   return (
-    <div className='p-2 ml-2 border border-white w-full h-[410px] overflow-y-scroll'>
-      {chatMessages.map((c) => (
-        <ChatMessage key={c.id} name={c.name} message={c.message}/>
-      ))}
-    </div>
-  )
+    <>
+      <div className="p-2 ml-2 rounded border border-white w-full h-[410px] overflow-y-scroll flex flex-col-reverse">
+        {chatMessages.map((c) => (
+          <ChatMessage key={c.id} name={c.name} message={c.message} />
+        ))}
+      </div>
+
+      <form className="w-[95%] p-2 flex" onSubmit={(e) => {
+        e.preventDefault();
+        console.log('on form submit', liveMessage);
+        dispatch( addMessage({
+          name: 'Chaitanya',
+          message: liveMessage,
+        })
+      )
+      setLiveMessage('');
+      }}>
+        <input
+          type="text"
+          placeholder="write live chat msg"
+          value={liveMessage}
+          onChange={(e) => {
+            setLiveMessage(e.target.value)
+          }}
+          className="w-full p-1 bg-black text-white  border border-white rounded-lg"
+        />
+        <button className='px-4 '><IoSend /></button>
+      </form>
+    </>
+  );
 }
 
 export default LiveChat;
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -23,6 +23,20 @@ var nameList = [
 ];
 
 export function generateRandomName() {
-   var finalName = nameList[Math.floor( Math.random() * nameList.length )];
-   return finalName;
+   return nameList[Math.floor( Math.random() * nameList.length )];
+   
 };
+
+export function makeRandomMessage(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+}
+
+console.log(makeRandomMessage(5));
