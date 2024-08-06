e3daaa3c6bfca39d3c91af824aa77d8d6518a121
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-08-06 22:52:21 +0530

    yt info

--- src/components/VideoInfo.js
+++ src/components/VideoInfo.js
@@ -29,9 +29,9 @@ const VideoInfo = ({ videoId }) => {
     <div className='ml-4 pt-2 max-w-[725px]'>
       <h1 className='text-2xl font-bold'>{snippet.title}</h1>
       <p className='mt-2'>{snippet.description}</p>
-      <p className='mt-2'>Views: {statistics.viewCount}</p>
-      <p className='mt-2'>Likes: {statistics.likeCount}</p>
-      <p className='mt-2'>Comments: {statistics.commentCount}</p>
+      <p className='mt-2'>Views {statistics.viewCount}</p>
+      <p className='mt-2'>Likes {statistics.likeCount}</p>
+      <p className='mt-2'>Comments {statistics.commentCount}</p>
     </div>
   );
 };
